5fed4c8955d1aea1416bc7db6d7c17d4205867b5
Gabriella <isgabriellacurious@gmail.com>
2020-10-14 21:16:27 +0100

    Initial commit

---
+++ MoniteerServer/Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace MoniteerServer
+{
+    public class Service
+    {
+        private readonly Timer _timer;
+
+        public Service()
+        {
+            _timer = new Timer(1000 /*millis*/) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Moniteer\Hearbeat.txt", lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
